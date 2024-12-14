869d516aa10360744f32e904cf172cf8a612afee
kaakabogu <kjadaku@outlook.com>
2024-12-14 14:58:11 -0600

    Changes to views, initi, and config py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,11 +67,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warning("Incorrect login credentials - Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+            app.logger.warning("Successful login attempt")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
@@ -82,19 +84,33 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
+        app.logger.warning("Authentication Failed")
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
-        cache = _load_cache()
+        def _load_cache():
+            # TODO: Load the cache from `msal`, if it exists
+            cache = msal.SerializableTokenCache()
+            if session.get('token_cache'):
+                cache.deserialize(session['token_cache'])
+            return cache
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+            request.args.get('code'),
+            scopes=Config.SCOPE,
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
+            app.logger.warning("Authentication Failed")
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
         login_user(user)
-        _save_cache(cache)
+        def _save_cache(cache):
+            # TODO: Save the cache, if it has changed
+            app.logger.warning("Successful login attempt")
+            if cache.has_state_changed:
+                session['token_cache'] = cache.serialize()
     return redirect(url_for('home'))
 
 @app.route('/logout')
